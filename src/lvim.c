30826d243b097ac98c557a8f4f0536975bc7ef6d
OmarCompiled <179285620+OmarCompiled@users.noreply.github.com>
2025-04-21 16:14:05 +0200

    Add command function and handle proper exit command

--- src/lvim.c
+++ src/lvim.c
@@ -1,7 +1,26 @@
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 #include <unistd.h>
 #include <ncurses.h>
+
+void
+get_command(const char input)
+{
+	noraw();
+	addch(':');
+	echo();
+	char *command = (char *)malloc(sizeof(char) * 100);
+	getstr(command);
+	if(!strcmp(command, "q")) {
+		exit(0);
+	} else {
+		addstr("Unkown command");
+	}
+	free(command);
+	raw();
+	noecho();
+}
 
 void
 clean_screen()
@@ -23,10 +42,15 @@ setup_screen()
 int
 main()
 {
+	int cur_y, cur_x;
+	int max_y, max_x;
+	cur_y = max_y = cur_x = max_x = 0;
+
 	setup_screen();
 	
 	char input;
-	while(input = getch()) {
+	while((input = getch())) {
+		input == ':' ? get_command(input) : 0;
 		printw("%c", input);
 		refresh();
 	}
