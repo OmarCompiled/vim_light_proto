22528bc68e1c599bf7db1374d522b60220a996c1
OmarCompiled <179285620+OmarCompiled@users.noreply.github.com>
2025-04-23 18:29:26 +0200

    Add more interactiveness

---
+++ src/config.h
@@ -0,0 +1,17 @@
+#include <ncurses.h>
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* coordinate variables */
+static int max_y;
+static int max_x;
+int 	 current_y = 0;
+int 	 current_x = 0;
+
+/* command prompt string  */
+static char* command;
+
+WINDOW* status_bar;
+
+#endif
--- src/lvim.c
+++ src/lvim.c
@@ -1,10 +1,11 @@
+#include <ctype.h>
+#include <ncurses.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 #include <unistd.h>
-#include <ncurses.h>
 
-#include "coords.h"
+#include "config.h"
 
 #define MAX_Y max_y-2
 #define MAX_X max_x-2
@@ -19,6 +20,17 @@ back_to_position()
 }
 
 void
+validate_command()
+{
+	if(!strcmp(command, "q")) {
+		exit(0);
+	} else {
+	  move(MAX_Y, 0);
+		clrtoeol();
+	}
+}
+
+void
 get_command(const char input)
 {
 	move(MAX_Y, 0);
@@ -27,13 +39,19 @@ get_command(const char input)
 	echo();
 	command = (char *)malloc(sizeof(char) * 100);
 	getstr(command);
-	if(!strcmp(command, "q")) {
-		exit(0);
-	} else {
-	  move(MAX_Y, 0);
-		clrtoeol();
-	}
+	validate_command();
 	back_to_position();
+}
+
+bool
+check_for_command(const char input)
+{
+	if(input == ':'){
+		get_command(input);
+		return true;
+	} else {
+		return false;
+	}
 }
 
 void
@@ -47,7 +65,8 @@ void
 setup_screen()
 {
 	initscr();
-	erase();
+	status_bar = newwin(1, COLS, LINES-1, 0);
+
 	raw();
 	noecho();
 	keypad(stdscr, TRUE);
@@ -57,17 +76,26 @@ setup_screen()
 	atexit(clean_screen);
 }
 
+void
+update_status_bar()
+{
+	werase(status_bar);
+	wprintw(status_bar, "%s \t\t\t\t Ln %d, Col %d", "HI", current_y + 1, current_x + 1);
+	wrefresh(status_bar);
+}
+
 int
 main()
 {
 	setup_screen();
+
 	char input;
-	while((input = getch())) {
-		if(input == ':'){
-			getyx(stdscr, current_y, current_x);
-			get_command(input);
+	while(input = getch()) {
+		getyx(stdscr, current_y, current_x);
+		if(check_for_command(input)) {
 			continue;
 		}
+		update_status_bar();
 		printw("%c", input);
 		refresh();
 	}
